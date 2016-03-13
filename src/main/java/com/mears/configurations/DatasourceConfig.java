2e2dffdb50b5642a56a7a20b424e424716c3bab3
Phuc Bui <phuc_bui7718@yahoo.com>
2016-03-13 16:00:45 -0400

    Some stuff

--- src/main/java/com/mears/configurations/DatasourceConfig.java
+++ src/main/java/com/mears/configurations/DatasourceConfig.java
@@ -1,4 +1,4 @@
-package com.mears.configuration;
+package com.mears.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
